a80c32eff8aade2a429b200080bac6ed5ff04f49
RadRasyad <rafidanyr21@gmail.com>
2022-10-13 09:37:33 +0700

    SplashScreen

---
+++ app/src/main/java/com/hacktiv8/joyshop/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.hacktiv8.joyshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.hacktiv8.joyshop.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ActivitySplashScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int time = 3000;
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, time);
+    }
+}
