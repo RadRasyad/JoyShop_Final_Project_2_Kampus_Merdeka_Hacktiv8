c86df80d46c03b62d651ea9735677c829dde7581
RadRasyad <rafidanyr21@gmail.com>
2022-10-13 09:13:40 +0700

    Initial commit

---
+++ app/src/main/java/com/hacktiv8/joyshop/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hacktiv8.joyshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
