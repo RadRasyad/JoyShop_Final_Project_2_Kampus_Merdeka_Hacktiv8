c7773c469ed17d6ec76905d9c2f9f8f19c53a488
Syaifun Nadhif <88659346+SyaifunNadhif@users.noreply.github.com>
2022-10-18 10:09:39 +0700

    done login user

--- app/src/main/java/com/hacktiv8/joyshop/ui/MainActivity.java
+++ app/src/main/java/com/hacktiv8/joyshop/ui/MainActivity.java
@@ -1,17 +1,25 @@
 package com.hacktiv8.joyshop.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.hacktiv8.joyshop.databinding.ActivityMainBinding;
+import com.hacktiv8.joyshop.model.User;
 import com.hacktiv8.joyshop.preferences.UserPreference;
 import com.hacktiv8.joyshop.ui.admin.AdminHomeActivity;
 import com.hacktiv8.joyshop.ui.admin.AdminLoginActivity;
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private FirebaseAuth mAuth;
     private UserPreference preference;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         preference = new UserPreference(this);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
 
         binding.btnRegister.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RegisterUserActivity.class);
@@ -56,6 +66,52 @@ public class MainActivity extends AppCompatActivity {
 
     private void loginUser() {
         // TODO lakukan sesuatu untuk login user
+        String username = binding.inputUsername.getText().toString().trim();
+        String password = binding.inputPassword.getText().toString().trim();
+
+        databaseReference.child(username).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean isExcist = snapshot.exists();
+                if(isExcist) {
+                    User user = snapshot.getValue(User.class);
+                    if(user.getRole().equals("2")){
+                        mAuth.signInWithEmailAndPassword(user.getEmail(), password)
+                                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if(task.isSuccessful()){
+                                            User toSave = new User();
+                                            toSave.setuId(user.getuId());
+                                            toSave.setUsername(user.getUsername());
+                                            toSave.setEmail(user.getEmail());
+                                            toSave.setPhone(user.getPhone());
+                                            toSave.setRole(user.getRole());
+                                            String role = user.getRole();
+                                            saveUser(toSave);
+                                            reload(role);
+
+                                        }else{
+                                            Log.w("MainActivity", "signInWithEmail:failure", task.getException());
+                                            Toast.makeText(MainActivity.this, "Password Salah",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    }else{
+                        Toast.makeText(MainActivity.this, "Anda Bukan User", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this,"Akun tidak ditemukan", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -81,8 +137,14 @@ public class MainActivity extends AppCompatActivity {
 //            Intent intent = new Intent(MainActivity.this, AdminHomeActivity.class);
 //            startActivity(intent);
         } if ("2".equals(role)) {
-//            Intent intent = new Intent(MainActivity.this, AdminHomeActivity.class);
-//            startActivity(intent);
+            Intent intent = new Intent(MainActivity.this, AdminHomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
         }
+    }
+
+    private void saveUser(User user) {
+        UserPreference userPreference = new UserPreference(MainActivity.this);
+        userPreference.setUserPref(user);
     }
 }
